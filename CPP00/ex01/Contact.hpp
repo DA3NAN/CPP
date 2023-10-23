2a78ddf5bb28969c8cf16fa3f2e8eb09c8ffeac5
DA3NAN <adnane.aitmalek20@gmail.com>
2023-10-23 14:42:12 +0100

    damn all makefiles were relinking

--- CPP00/ex01/Contact.hpp
+++ CPP00/ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: aait-mal <aait-mal@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 19:02:21 by aait-mal          #+#    #+#             */
-/*   Updated: 2023/10/19 17:31:11 by aait-mal         ###   ########.fr       */
+/*   Updated: 2023/10/23 14:36:54 by aait-mal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 class Contact {
 	private:
 		std::string firstName, lastName, nickName, phoneNumber, darkestSecret;
-	
+
 	public:
 		void	setData(std::string firstName, std::string lastName, std::string nickName,
 				std::string phoneNumber, std::string darkestSecret);
