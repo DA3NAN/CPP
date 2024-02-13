0c4c5f1b366dcdd1e54debf73e54d2dc0bdbeb9c
DA3NAN <adnane.aitmalek20@gmail.com>
2024-02-13 17:59:49 +0100

    CPP08 EX00 Done

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aait-mal <aait-mal@student.42.fr>          +#+  +:+       +#+        */
+/*   By: adnane <adnane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 15:02:38 by aait-mal          #+#    #+#             */
-/*   Updated: 2024/02/13 15:19:04 by aait-mal         ###   ########.fr       */
+/*   Updated: 2024/02/13 17:55:54 by adnane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,14 @@
 # include <vector>
 # include <list>
 # include <deque>
-# include <set>
-# include <map>
-# include <functional>
 
 template <typename T>
 int easyfind(T &container, int n)
 {
-	typename T::iterator it;
-	it = std::find(container.begin(), container.end(), n);
+	typename T::iterator it = std::find(container.begin(), container.end(), n);
 	if (it == container.end())
 		throw std::runtime_error("Not found");
 	return *it;
 }
-
-// template <typename T>
-// int easyfind(T &container, int n)
-// {
-// 	typename T::const_iterator it;
-// 	it = std::find(container.begin(), container.end(), n);
-// 	if (it == container.end())
-// 		throw std::exception();
-// 	return *it;
-// }
 
 #endif
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aait-mal <aait-mal@student.42.fr>          +#+  +:+       +#+        */
+/*   By: adnane <adnane@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 15:05:06 by aait-mal          #+#    #+#             */
-/*   Updated: 2024/02/13 15:14:16 by aait-mal         ###   ########.fr       */
+/*   Updated: 2024/02/13 17:59:23 by adnane           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,16 @@ int main()
 	std::vector<int> v;
 	std::list<int> l;
 	std::deque<int> d;
-	std::set<int> s;
-	std::map<int, int> m;
 
 	for (int i = 0; i < 10; i++) {
 		v.push_back(i);
 		l.push_back(i);
 		d.push_back(i);
-		s.insert(i);
-		m.insert(std::pair<int, int>(i, i));
 	}
 	try {
 		std::cout << easyfind(v, 5) << std::endl;
 		std::cout << easyfind(l, 5) << std::endl;
 		std::cout << easyfind(d, 5) << std::endl;
-		std::cout << easyfind(s, 5) << std::endl;
-		std::cout << easyfind(m, 5) << std::endl;
 	} catch(const std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
